7cd2a9139087a39d93b07a898c38e31ff6312459
shivamsoy16 <shivamsoy16@gmail.com>
2021-12-18 13:26:44 -0800

    v1.0.0

--- app/src/main/java/com/example/navygayion13/MainActivity.java
+++ app/src/main/java/com/example/navygayion13/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.navygayion13;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -15,6 +18,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Fragment fragment = new TableFragment();
+        androidx.fragment.app.FragmentManager fm = getSupportFragmentManager();
+        androidx.fragment.app.FragmentTransaction transaction = fm.beginTransaction();
+        transaction.replace(R.id.contentFragment, fragment);
+        transaction.commit();
 
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -23,13 +31,28 @@ public class MainActivity extends AppCompatActivity {
                 switch (item.getItemId()) {
                     case R.id.btm_table:
                         Toast.makeText(MainActivity.this, "Periodic Table", Toast.LENGTH_SHORT).show();
-                        break;
+                        Fragment fragment = new TableFragment();
+                        androidx.fragment.app.FragmentManager fm = getSupportFragmentManager();
+                        androidx.fragment.app.FragmentTransaction transaction = fm.beginTransaction();
+                        transaction.replace(R.id.contentFragment, fragment);
+                        transaction.commit();
+                        return true;
                     case R.id.btm_list:
                         Toast.makeText(MainActivity.this, "List", Toast.LENGTH_SHORT).show();
-                        break;
+                        Fragment fragment2 = new ListFragment();
+                        androidx.fragment.app.FragmentManager fm2 = getSupportFragmentManager();
+                        androidx.fragment.app.FragmentTransaction transaction2 = fm2.beginTransaction();
+                        transaction2.replace(R.id.contentFragment, fragment2);
+                        transaction2.commit();
+                        return true;
                     case R.id.btm_trend:
                         Toast.makeText(MainActivity.this, "Trend", Toast.LENGTH_SHORT).show();
-                        break;
+                        Fragment fragment3 = new TrendFragment();
+                        androidx.fragment.app.FragmentManager fm3 = getSupportFragmentManager();
+                        androidx.fragment.app.FragmentTransaction transaction3 = fm3.beginTransaction();
+                        transaction3.replace(R.id.contentFragment, fragment3);
+                        transaction3.commit();
+                        return true;
                 }
                 return true;
             }
